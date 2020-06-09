32e640b8936a0cd5d4634381c34461ddb9a5aa19
Matthew Stuhr <36904235+MatthewStuhr@users.noreply.github.com>
2020-06-09 13:31:21 -0700

    Create constants.py
    adding the phrases to be guessed

---
+++ phrasehunter/constants.py
@@ -0,0 +1,6 @@
+stored_phrases=['Life is like a box of chocolates',
+                'There is no trying',
+                'May the force be with you',
+                'You have to see the matrix for yourself',
+                'You talking to me',
+               ]
