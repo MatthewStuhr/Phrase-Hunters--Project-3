1063d808e77d2defdf54b39a8e52392d793aa684
Matthew Stuhr <36904235+MatthewStuhr@users.noreply.github.com>
2020-06-09 13:35:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 # Import your Game class
-
+from phrasehunter.game import Game
+from phrasehunter.constants import stored_phrases
 # Create your Dunder Main statement.
 
 # Inside Dunder Main:
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
+if __name__ == '__main__':
+    game = Game(stored_phrases)
+    try:
+            game.run_game()
+    except KeyboardInterrupt:
+            print("\nThanks for playing!")
