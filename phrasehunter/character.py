51e866a1495c3dd8d897b9622bb499eb07c96ab0
Matthew Stuhr <36904235+MatthewStuhr@users.noreply.github.com>
2020-06-12 11:21:35 -0700

    Update character.py
    added a raise Value Error if the player enters more than 1 letter. fixed some syntax errors.

--- phrasehunter/character.py
+++ phrasehunter/character.py
@@ -3,13 +3,15 @@ class Character:
     def __init__(self, original, was_guessed=False):
         self.original = original
         self.was_guessed = was_guessed
+        if len(original) != 1:
+            raise ValueError("Please enter 1 letter at a time.")
         
     def new_guess(self, guess):
         """ An instance method that will take a single string character guess as an argument when its 
         called. The job of this instance method is to update the instance attribute storing the boolean 
         value, if the guess character is an exact match to the instance attribute storing the original 
         char passed in when the Character object was created. """
-        if self.was_guessed == False and guess = self.original:
+        if self.guess.upper() == self.original.upper():
             self.was_guessed = True
             
             
@@ -18,9 +20,9 @@ class Character:
         """ An instance method that when called, will show the original character if the instance 
         attribute was_guessed is True. Otherwise, the instance method should show an _ or underscore 
         character to act as a hidden placeholder character. """
-        if self.was_guessed == True
+        if self.was_guessed == True:
             return character.format(self.original)
-        elif self.original = ' ':
+        elif self.original == ' ':
             return character.format(' ')
         else:
             return character.format('_')
